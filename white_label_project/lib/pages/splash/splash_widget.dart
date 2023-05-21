345556a70dcb4ca01f2c8d630053f1b423b52dc0
Harry Yan <harryyan1238@gmail.com>
2023-05-21 21:33:42 +1200

    add ScreenSizeConfig

---
+++ white_label_project/lib/pages/splash/splash_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashWidge extends StatefulWidget {
+  const SplashWidge({super.key});
+
+  @override
+  State<SplashWidge> createState() => _SplashWidgeState();
+}
+
+class _SplashWidgeState extends State<SplashWidge> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
