c60a6ec89fca4d6d0e14020a301c6231e51e74f1
Harry Yan <harryyan1238@gmail.com>
2023-05-23 15:58:25 +1200

    update container page

--- white_label_project/lib/pages/splash/container/container_page.dart
+++ white_label_project/lib/pages/splash/container/container_page.dart
@@ -66,14 +66,54 @@ class _ContainerPageState extends State<ContainerPage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-
-  @override
   void didUpdateWidget(ContainerPage oldWidget) {
     super.didUpdateWidget(oldWidget);
     debugPrint('App Container Did Update...');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          _getPagesWidget(0),
+          _getPagesWidget(1),
+          _getPagesWidget(2),
+          _getPagesWidget(3),
+          _getPagesWidget(4)
+        ],
+      ),
+      backgroundColor: Color.fromARGB(255, 248, 248, 248),
+      bottomNavigationBar: BottomNavigationBar(
+        items: itemList,
+        onTap: (int index) {
+          ///这里根据点击的index来显示，非index的page均隐藏
+          setState(() {
+            _currentIndex = index;
+            //这个是用来控制比较特别的shopPage中WebView不能动态隐藏的问题
+            // shopPageWidget.setShowState(pages.indexOf(shopPageWidget) == _selectIndex);
+          });
+        },
+        //图标大小
+        iconSize: 24,
+        //当前选中的索引
+        currentIndex: _currentIndex,
+        //选中后，底部BottomNavigationBar内容的颜色(选中时，默认为主题色)（仅当type: BottomNavigationBarType.fixed,时生效）
+        fixedColor: Color.fromARGB(255, 0, 188, 96),
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  //Stack+Offstage to solve page reset issue
+  Widget _getPagesWidget(int index) {
+    return Offstage(
+      offstage: _currentIndex != index,
+      child: TickerMode(
+        enabled: _currentIndex == index,
+        child: pages[index],
+      ),
+    );
   }
 }
 
