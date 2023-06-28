a49ec9f904f70e351f34bed2c00b4e2553c1d1cc
Harry Yan <harryyan1238@gmail.com>
2023-06-28 19:21:10 +1200

    finish/refactor tabcontroller

--- white_label_project/lib/widgets/home_tabbar.dart
+++ white_label_project/lib/widgets/home_tabbar.dart
@@ -1,7 +1,11 @@
 import "package:flutter/material.dart";
 
+// ignore: must_be_immutable
 class HomePageTabBar extends StatefulWidget {
-  const HomePageTabBar({super.key});
+  TabController tabController;
+  List<Widget> tabs;
+
+  HomePageTabBar(Key key, this.tabController, this.tabs) : super(key: key);
 
   @override
   State<HomePageTabBar> createState() => _HomePageTabBarState();
@@ -14,6 +18,7 @@ class _HomePageTabBarState extends State<HomePageTabBar> {
   @override
   void initState() {
     super.initState();
+
     selectColor = Colors.black;
     unselectedColor = const Color.fromARGB(255, 117, 117, 117);
     selectStyle = TextStyle(fontSize: 18, color: selectColor);
@@ -21,7 +26,26 @@ class _HomePageTabBarState extends State<HomePageTabBar> {
   }
 
   @override
+  void dispose() {
+    widget.tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+      child: TabBar(
+        tabs: widget.tabs,
+        isScrollable: true,
+        controller: widget.tabController,
+        indicatorColor: selectColor,
+        labelColor: selectColor,
+        labelStyle: selectStyle,
+        unselectedLabelColor: unselectedColor,
+        unselectedLabelStyle: unselectedStyle,
+        indicatorSize: TabBarIndicatorSize.label,
+      ),
+    );
   }
 }
